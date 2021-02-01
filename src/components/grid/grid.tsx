2416e008d438cc5b176551ffa6cf9fbf33d9be88
florian gaebler <floriangaebler87@gmail.com>
2021-02-01 12:51:39 +0100

    added Colorpicker

--- src/components/grid/grid.tsx
+++ src/components/grid/grid.tsx
@@ -3,14 +3,44 @@
 import React, { FC, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Rootstate } from '../../redux/store/store';
-import {TileDiv} from '../tile/tile';
+import { TileDiv } from '../tile/tile';
 import { SetColorAction, SetTilesAction } from '../../redux/reducer/grid';
 import { ITile } from '../../interfaces/Tile';
+import styled from 'styled-components';
+
+const Container = styled.span`
+  display: inline-flex;
+  align-items: center;
+  width: 150px;
+  padding: 4px 12px;
+  border: 1px solid black;
+  border-radius: 4px;
+
+  input[type='color'] {
+    border: none;
+    background: none;
+    &::-webkit-color-swatch-wrapper {
+      border: 1px solid black;
+      border-radius: 4px;
+    }
+  }
+
+  input[type='text'] {
+    border: none;
+    width: 100%;
+    font-size: 14px;
+  }
+  input[type='number'] {
+    border: none;
+    width: 100%;
+    font-size: 14px;
+  }
+`;
 
 const Grid: FC = () => {
   const dispatch = useDispatch();
-  const [tile, settile] = useState(0);
-  const [color, setColor] = useState('black')
+  const [tile, settile] = useState(64);
+  const [color, setColor] = useState('black');
 
   const tiles = useSelector<Rootstate, ITile[]>((state) => state.board.tiles);
 
@@ -18,18 +48,36 @@ const Grid: FC = () => {
     dispatch(SetTilesAction(tile));
   }, [tile]);
 
-  const handleColorState = (tileId: string) => {
-    dispatch(SetColorAction(tileId)) 
-  }
+  const handleTileState = (tileId: string) => {
+    dispatch(SetColorAction(tileId));
+  };
 
-  const handleTileState = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleTileStateChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     return settile(parseInt(event.currentTarget.value));
   };
+
+  const handleColorState = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setColor(event.currentTarget.value);
+  };
+
+  // const colorRange = (start: number, end: number, length = end - start + 1) => {
+  //   Array.from({ length }, (_, i) => (
+  //     <div onClick={handleColorState()} className={`color-${start + i}`}></div>
+  //   ));
+  // };
 
   return (
     <div className="board">
       <div className="form">
-        <input type="number" onChange={handleTileState} />
+        <Container>
+          <input type="number" onChange={handleTileStateChange} value={tile} />
+        </Container>
+        <Container>
+          <input type="color" value={color} onChange={handleColorState} />
+          <input type="text" value={color} />
+        </Container>
       </div>
       <div className="grid">
         {tiles.map((item) => {
@@ -38,7 +86,7 @@ const Grid: FC = () => {
               key={item.id}
               id={item.id}
               color={item.hasColor ? color : 'white'}
-              onClick={() => handleColorState(item.id)}
+              onClick={() => handleTileState(item.id)}
               hasColor={item.hasColor}
             />
           );
--- src/components/tile/tile.tsx
+++ src/components/tile/tile.tsx
@@ -1,14 +1,9 @@
-import React, { FC, useState } from 'react';
-import styles from './tile.module.css'
 import { ITile } from '../../interfaces/Tile';
 import styled from 'styled-components';
-import { idText } from 'typescript';
-
-
 
 export const TileDiv = styled.div<ITile>`
-  background: ${props => props.color};
+  background: ${(props) => props.color};
   width: 10vh;
   height: 10vh;
   border: 2px solid black;
-`
+`;
